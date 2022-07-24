d368523ad8bb5ac1c9ffb50dd39b6911df9e2788
tjumma <tjumma@solcery.xyz>
2022-07-25 02:17:45 +0300

    roadmap icons

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,9 +1,19 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Splide } from "@splidejs/react-splide";
 import { RoadmapCard } from "./RoadmapCard";
 
 import road_map_line from "../assets/img/roadmap/roadmap_line.png";
-import roadmap_empty from "../assets/img/roadmap/roadmap_empty.svg";
+import roadmap_hackathon from "../assets/img/roadmap/roadmap_hackathon.svg";
+import roadmap_game from "../assets/img/roadmap/roadmap_game.svg";
+import roadmap_round from "../assets/img/roadmap/roadmap_round.svg";
+import roadmap_vm from "../assets/img/roadmap/roadmap_vm.svg";
+import roadmap_sry from "../assets/img/roadmap/roadmap_sry.svg";
+import roadmap_sage from "../assets/img/roadmap/roadmap_sage.svg";
 import roadmap_forge from "../assets/img/roadmap/roadmap_forge.svg";
+import roadmap_ratings from "../assets/img/roadmap/roadmap_ratings.svg";
+import roadmap_mobile from "../assets/img/roadmap/roadmap_mobile.svg";
+import roadmap_p2p from "../assets/img/roadmap/roadmap_p2p.svg";
+import roadmap_mm from "../assets/img/roadmap/roadmap_mm.svg";
+import roadmap_achievements from "../assets/img/roadmap/roadmap_achievements.svg";
 
 export const Roadmap = () => {
     return (
@@ -33,21 +43,21 @@ export const Roadmap = () => {
                         },
                     }}>
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_hackathon}
                         heading="Solana Season Hackathon"
                         time="June 2021"
                         description="Honorable mention and a proof of concept for a fully on-chain game"
                         link='https://solana.com/news/announcing-winners-of-the-solana-season-hackathon'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_game}
                         heading="Summoner"
                         time="August 2021"
                         description="The very first game made with Solcery. A deckbuilding PvP game with a fully on-chain logic."
                         link='https://docs.solcery.xyz/games/summoner'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_round}
                         heading="Seed Round"
                         time="September 2021"
                         description="Funding needed to bootstrap the team secured."
@@ -55,55 +65,55 @@ export const Roadmap = () => {
                         newTab={false}
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_hackathon}
                         heading="Ignition Hackathon"
                         time="October 2021"
                         description="Honorable mention and a proof of concept for onboarding any Solana NFT into Solcery games."
                         link='https://solana.com/news/solana-ignition-hackathon-winners'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_game}
                         heading="Solitary"
                         time="November 2021"
                         description="A single-player roguelike deckbuilder that pioneered external NFT onboarding."
                         link='https://docs.solcery.xyz/games/solitary'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_vm}
                         heading="Solcery Virtual Machine"
                         time="December 2021"
                         description="An on-chain protocol meant to be the ultimate source of consensys around any game state or any game result."
                         link='https://docs.solcery.xyz/protocols/solcery-vm'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_sage}
                         heading="Sage"
                         time="March 2022"
                         description="Solcery game editor itself - a zero-code engine and an easy-to-use content management system."
                         link='https://docs.solcery.xyz/sage'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_game}
                         heading="Eclipse"
                         time="August 2022"
                         description="A single-player resource management strategy game with a focus on oboarding NFTs and community-driven development."
                         link='https://docs.solcery.xyz/games/eclipse'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_round}
                         heading="Strategic Round"
                         time="Q3 2022"
                         description="A round of investment to extend the team and speed up bringing our vision to life."
                     //TODO: link to the deck
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_ratings}
                         heading="Ratings"
                         time="Q3 2022"
                         description="Ratings and Leaderboards protocols to bring competitiveness and reward the best."
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_sry}
                         heading="SRY token launch"
                         time="Q4 2022"
                         description="The launch of SRY token - the essential piece in decentralizing Solcery ecosystem."
@@ -116,26 +126,26 @@ export const Roadmap = () => {
                         link='https://docs.solcery.xyz/protocols/solcery-forge'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_mobile}
                         heading="Mobile client"
                         time="Q1 2023"
                         description="Solcery starts its journey with SAGA and SMSS - the mobile-native crypto stack."
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_p2p}
                         heading="P2P protocol"
                         time="Q1 2023"
                         description="A protocol for direct trustless communication between players clients essential for making our games faster and cheaper."
                         link='https://docs.solcery.xyz/protocols/p2p'
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_mm}
                         heading="Matchmaking"
                         time="Q1 2023"
                         description="A robust customizable protocol which games can utilize for matching their players by any property."
                     />
                     <RoadmapCard
-                        img={roadmap_empty}
+                        img={roadmap_achievements}
                         heading="Achievements"
                         time="Q2 2023"
                         description="A system of game-specific and ecosystem-wide achievements."
